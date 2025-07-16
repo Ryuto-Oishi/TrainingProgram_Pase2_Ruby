36053cabff57e3456e67661535ea8c3df1f53d10
大石 琉翔 <ryuto.oishi@rizapgroup.com>
2025-07-16 11:38:21 +0900

    #3 課題2-1の修正(2)

--- kadai2/class_only.rb
+++ kadai2/class_only.rb
@@ -1,16 +1,4 @@
 class ClassOnly
-    attr_accessor :id
-    attr_accessor :name
-    attr_accessor :ruby
-    attr_accessor :sex
-    attr_accessor :tel
-    attr_accessor :mobile
-    attr_accessor :mail
-    attr_accessor :zip
-    attr_accessor :address1
-    attr_accessor :address2
-    attr_accessor :address3
-    attr_accessor :address4
-    attr_accessor :address5
-    attr_accessor :birthday
+    attr_accessor :id, :name, :ruby, :sex, :tel, :mobile, :mail, :zip,
+    :address1, :address2, :address3, :address4, :address5, :birthday
 end
