6d93c6878e76df9ecea7c2df837cf0effaf6d287
大石 琉翔 <ryuto.oishi@rizapgroup.com>
2025-07-16 11:26:02 +0900

    #3 課題2-1の修正

--- kadai2/class_only.rb
+++ kadai2/class_only.rb
@@ -1,22 +1,16 @@
-class Class_Only
-    def initialize(row)
-        @id = row['no'],
-        @name = row['namae'],
-        @ruby = row['rubi'],
-        @sex = row['seibetu'],
-        @tel = row['denwa'],
-        @mobile = row['keitai'],
-        @mail = row['mair'],
-        @zip = row['yuubinbango'],
-        @address1 = row['jusho1'],
-        @address2 = row['jusho2'],
-        @address3 = row['jusho3'],
-        @address4 = row['jusho4'],
-        @address5 = row['jusho5'],
-        @birthday = row['tanjobi']
-    end
-
-    def name
-        puts @name
-    end
+class ClassOnly
+    attr_accessor :id
+    attr_accessor :name
+    attr_accessor :ruby
+    attr_accessor :sex
+    attr_accessor :tel
+    attr_accessor :mobile
+    attr_accessor :mail
+    attr_accessor :zip
+    attr_accessor :address1
+    attr_accessor :address2
+    attr_accessor :address3
+    attr_accessor :address4
+    attr_accessor :address5
+    attr_accessor :birthday
 end
--- kadai2/kadai2-1.rb
+++ kadai2/kadai2-1.rb
@@ -2,9 +2,24 @@ require "csv"
 require_relative "./class_only.rb"
 data = Array.new
 CSV.foreach("./personal_infomation.csv", headers: true) do |row|
-    personal_data = Class_Only.new(row)
+    personal_data = ClassOnly.new
+    personal_data.id = row['no']
+    personal_data.name = row['namae']
+    personal_data.ruby = row['rubi']
+    personal_data.sex = row['seibetu']
+    personal_data.tel = row['denwa']
+    personal_data.mobile = row['keitai']
+    personal_data.mail = row['mair']
+    personal_data.zip = row['yuubinbango']
+    personal_data.address1 = row['jusho1']
+    personal_data.address2 = row['jusho2']
+    personal_data.address3 = row['jusho3']
+    personal_data.address4 = row['jusho4']
+    personal_data.address5 = row['jusho5']
+    personal_data.birthday = row['tanjobi']
+
     data.push(personal_data)
 end
 data.each do |personal_data|
-    personal_data.name
+    p personal_data.name
 end
