16f15b633750f4fabd36efdc333027adf770b7de
大石 琉翔 <ryuto.oishi@rizapgroup.com>
2025-07-22 11:01:12 +0900

    #7 課題3を解いた

---
+++ kadai3/kadai3.rb
@@ -0,0 +1,37 @@
+require "csv"
+require "json"
+require_relative "./user.rb"
+users = Array.new
+CSV.foreach("./personal_infomation.csv", headers: true) do |row|
+    personal_data = User.new
+    personal_data.id = row['no']
+    personal_data.name = row['namae']
+    personal_data.ruby = row['rubi']
+    personal_data.sex = row['seibetu']
+    personal_data.tel = row['denwa']
+    personal_data.mobile = row['keitai']
+    personal_data.mail = row['mair']
+    personal_data.zip = row['yuubinbango']
+    personal_data.address1 = row['jusho1']
+    personal_data.address2 = row['jusho2']
+    personal_data.address3 = row['jusho3']
+    personal_data.address4 = row['jusho4']
+    personal_data.address5 = row['jusho5']
+    personal_data.birthday = row['tanjobi']
+
+    users.push(personal_data)
+end
+
+json_data = []
+File.open("./user.json", mode = "w") do |file|
+    users.each do |user|
+        json_data.push({
+            "id": user.id,
+            "name": user.name,
+            "address": user.address,
+            "birthday": user.birthday,
+            "age": user.age
+        })
+    end
+    file.puts(JSON.pretty_generate(json_data), file)
+end
