e2b17e3e4e2fd16b7aac696d3f294109fdd6db27
大石 琉翔 <ryuto.oishi@rizapgroup.com>
2025-07-23 16:07:20 +0900

    #13 課題6を解いた

---
+++ kadai6/user.rb
@@ -0,0 +1,21 @@
+require "date"
+class User
+    attr_accessor :id, :name, :ruby, :sex, :tel, :mobile, :mail, :zip,
+    :address1, :address2, :address3, :address4, :address5, :birthday
+
+    def address
+        @address = [@address1, @address2, @address3, @address4, @address5].join(" ")
+    end
+    def age
+        today = Date.today
+        birthday = Date.parse(@birthday)
+        @age = ((today.strftime('%Y%m%d').to_f - birthday.strftime('%Y%m%d').to_f) / 10000).to_i
+    end
+
+    def birthday=(text)
+        @birthday = text
+        if self.age < 18
+            raise AgeError.new("エラー：18歳未満のデータが挿入されました")
+        end
+    end
+end
